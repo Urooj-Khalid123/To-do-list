9ee4c02446e34afb2146e1322713d930480a494c
Urooj Muhammad Khalid <161545173+Urooj-Khalid123@users.noreply.github.com>
2024-04-10 20:41:54 +0500

    this is to-do-list

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while(condition){
+   
+    let addTask = await inquirer.prompt(
+        [
+            {
+                name: "todo",
+                message: "What you want to add in your Todos?",
+                type: "input"
+                
+            },
+    
+            {
+                name: "addMore",
+                message: "Are you sure you want to add more Todos?",
+                type: "confirm",
+                default: "false"
+            }
+        ]
+        );
+    
+    
+        todos.push(addTask.todo);
+        condition = addTask.addMore;
+        console.log(todos);
+
+}
+
